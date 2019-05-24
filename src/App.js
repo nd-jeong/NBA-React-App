a2f1564f1798fd3e6900db4587f9ffaff679a120
Andy Jeong <unjin89@gmail.com>
2019-05-24 12:11:13 -0400

    mapped and rendered all team names in teamList component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,35 @@ import React, {Component} from 'react';
 import './App.css';
 import axios from 'axios';
 import {Route, Link} from 'react-router-dom';
+import TeamList from './components/TeamList'
 
 class App extends Component {
     constructor() {
         super();
 
         this.state = {
-
+            allTeamsList: []
         }
     }
 
     async componentDidMount() {
-        const fetchTeamData = await axios.get('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=NBA');
+        await axios.get('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=NBA')
+            .then(res => {
+                this.setState({
+                    allTeamsList: res.data
+                })
+            })
         const fetchPlayerData = await axios.get('https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=Toronto Raptors');
-        console.log(fetchPlayerData)
     }
 
     render() {
+        const teamArray = this.state.allTeamsList.teams;
+        
         return(
             <div>
-                
+                <TeamList
+                    teamArray={teamArray}
+                />
             </div>
         )
     }
--- src/components/TeamList.js
+++ src/components/TeamList.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const TeamList = () => {
-    return(
-        <div></div>
-    )
+class TeamList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const teams = this.props.teamArray;
+        console.log(teams)
+        return(
+            <div>
+                {teams && teams.map(team => 
+                    <h2>{team.strTeam}</h2>
+                )}
+            </div>
+        )
+    }  
 }
 
 export default TeamList
